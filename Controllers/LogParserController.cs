a9d50cdaa3078ed5abcb7a3eb66477ee7ffc77e7
Евгений Михайлюков <mihaylyukov99@gmail.com>
2023-05-16 18:23:32 +0600

    создал контроллер

---
+++ Controllers/LogParserController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LogParser.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class LogParserController : ControllerBase
+{
+    // GET: api/<LogParserController>
+    [HttpGet]
+    public IEnumerable<string> Get()
+    {
+        return new[] { "value1", "value2" };
+    }
+}
